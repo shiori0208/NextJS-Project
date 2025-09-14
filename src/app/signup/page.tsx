7f0303007c54ffec3c4c8d066d7e739dfab959a7
shiori0208 <shioriuem02@gmail.com>
2025-09-14 22:40:53 +0530

    added and styled username input...

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -13,14 +13,20 @@ export default function SignupPage() {
   })
 
   const onSignup = async () => {
-
   }
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-amber-200 text-2xl">
-        SignUp
-      </h1>
+        SignUp </h1>
+        <hr />
+        <label htmlFor="username">Username:</label>
+        <input className="bg-gray-600 rounded-md p-2"
+        type="text" 
+        id="username"
+        value={user.username}
+        onChange={(e) => setUser({...user, username: e.target.value})}
+        placeholder="username" />
     </div>
   )
 }
