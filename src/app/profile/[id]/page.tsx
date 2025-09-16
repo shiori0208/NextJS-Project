7c8b7370ebf00807ebb6bdf3d5b53a4d639a8dbe
shiori0208 <shioriuem02@gmail.com>
2025-09-16 21:28:16 +0530

    edited user  profile page...

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -4,7 +4,10 @@ export default function UserProfile({params}: any)
         <div className="flex flex-col items-center justify-center min-h-screen py-2 text-2xl">
             <h1>Profile</h1>
             <hr />
-            <p>Profile Pic</p>
+            <p className="text-4xl">
+                Profile Page 
+                <span className="p-1 rounded-md bg-blue-400 text-black">{params.id}</span>
+            </p>
 
         
         </div>
