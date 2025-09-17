cbcd85f07859d9eecbcb12680c05b499717586f4
shiori0208 <shioriuem02@gmail.com>
2025-09-17 21:53:30 +0530

    making a POST request...

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,2 +1,19 @@
 import {connect} from "@/dbConfig/dbConfig"
+import User from "@/models/userModel"
+import { NextRequest, NextResponse } from "next/server"
+import bcrypt from "bcryptjs"
+
+connect() 
+
+export async function POST(request: NextRequest) {
+    try {
+        const reqBody = await request.json()
+        const {username, email, password} =reqBody
+
+    } catch (error : any) {
+        return NextResponse.json({error: error.message},
+            {status: 500})
+        
+    }
+}
 
