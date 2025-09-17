7a981e98ac2cb3085c5fe84f4e6b95fca4d4e5a0
shiori0208 <shioriuem02@gmail.com>
2025-09-17 21:59:33 +0530

    sdding standardized checks...

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,14 +1,26 @@
 import {connect} from "@/dbConfig/dbConfig"
 import User from "@/models/userModel"
 import { NextRequest, NextResponse } from "next/server"
-import bcrypt from "bcryptjs"
+import bcryptjs from "bcryptjs"
 
 connect() 
 
 export async function POST(request: NextRequest) {
     try {
         const reqBody = await request.json()
-        const {username, email, password} =reqBody
+        const {username, email, password} = reqBody
+
+        //check if user already exists
+        const user = await User.findOne({email})
+
+        if(user) {
+            return NextResponse.json({error: "User already exists"}, {status: 400})
+        }
+
+        //hash password
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
+
 
     } catch (error : any) {
         return NextResponse.json({error: error.message},
