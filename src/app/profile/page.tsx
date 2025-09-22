32ee4cc6dc584db65427668603a47f50fc2fcbd5
shiori0208 <shioriuem02@gmail.com>
2025-09-22 21:51:06 +0530

    adding a logout button in profile page...

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,20 @@
 export default function ProfilePage() 
 {
+    const logout = async () => {}
+
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2 text-2xl">
             <h1>Profile</h1>
             <hr />
             <p>Profile Pic</p>
 
-        
+            <hr />
+
+            <button
+            onClick={logout}
+            className="bg-pink-400 text-white px-4 py-2 rounded mt-4">
+                Logout
+            </button>
         </div>
     )
 }
