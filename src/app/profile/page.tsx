4a9ef1b654839baf7073b8785c5ed5caee35b215
shiori0208 <shioriuem02@gmail.com>
2025-09-23 11:24:20 +0530

    middleware page added...

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -30,7 +30,7 @@ export default function ProfilePage()
 
             <button
             onClick={logout}
-            className="bg-pink-400 text-white px-4 py-2 rounded mt-4">
+            className="bg-pink-400 text-white px-4 py-2 rounded mt-4 hover:bg-pink-500">
                 Logout
             </button>
         </div>
